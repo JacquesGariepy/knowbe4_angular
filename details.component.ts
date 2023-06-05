c30b51e32b735206e81a7f782e20bd8bb0ba291e
Jacques Gariépy <jacques.gariepy@outlook.com>
2023-06-05 15:45:39 -0400

    Create details.component.ts

---
+++ details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.css']
+})
+export class DetailsComponent {
+  // Propriétés et méthodes pour afficher les détails des inscriptions aux formations et des campagnes
+}
